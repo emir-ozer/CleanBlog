33da9a58a6b798cc24089ef3cdacfc3247a4ec15
Mehmet Emir ÖZER <94767595+emir-ozer@users.noreply.github.com>
2023-01-11 17:09:21 +0300

    Template engine inculeded, static files inserted

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-  const blog = {
-    id: 1,
-    title: 'Blog title',
-    description: 'Blog description',
-  };
-  res.send(blog);
+  res.render('index');
+});
+app.get('/index', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
 });
 
 const port = 3000;
